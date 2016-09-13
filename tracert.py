cf7ee0124e6bc2df505f7bed154afada61aa0117
Jason Grajeda <jason@grajeda.org>
2016-09-12 17:58:59 -0700

    Converted from single main function to a Class. May want to add functionality.

--- tracert.py
+++ tracert.py
@@ -1,58 +1,85 @@
+#! /usr/bin/env python3
+
 import socket
 import sys
+import os.path
+import time
 
 
-def main(dest_name):
-    dest_addr = socket.gethostbyname(dest_name)
-    host = socket.gethostbyname(socket.gethostname())
-    port = 33434
-    max_hops = 30
-    icmp = socket.getprotobyname('icmp')
-    udp = socket.getprotobyname('udp')
-    ttl = 1
-    print("Tracing: {0} ({1})".format(dest_name, dest_addr))
+class TraceRoute:
+    def __init__(self, destination_name):
+        self.destination_name = destination_name
+        self.destination_address = socket.gethostbyname(destination_name)
+        self.max_hops = 30
+        self.current_address = None
+        self.current_name = None
 
-    while True:
-        recv_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
-        send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, udp)
-        send_socket.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
+    def send(self):
+        host = socket.gethostbyname(socket.gethostname())
+        port = 33434
+        icmp = socket.getprotobyname('icmp')
+        udp = socket.getprotobyname('udp')
+        ttl = 1
 
-        recv_socket.settimeout(2.5)
-        recv_socket.bind((host, port))
-        send_socket.sendto(b"", (dest_addr, port))
-        curr_addr = None
-        curr_name = None
+        print("Tracing: {0} ({1})".format(self.destination_name,
+                                          self.destination_address))
+        while True:
+            receive_socket = socket.socket(socket.AF_INET,
+                                           socket.SOCK_RAW,
+                                           icmp)
+            receive_socket.settimeout(2.5)
+            receive_socket.bind((host, port))
 
-        try:
-            _, curr_addr = recv_socket.recvfrom(128)
-            curr_addr = curr_addr[0]
+            send_socket = socket.socket(socket.AF_INET,
+                                        socket.SOCK_DGRAM,
+                                        udp)
+            send_socket.setsockopt(socket.SOL_IP,
+                                   socket.IP_TTL,
+                                   ttl)
+            time_sent = time.time()
+            send_socket.sendto(b"", (self.destination_address, port))
             try:
-                curr_name = socket.gethostbyaddr(curr_addr)[0]
+                _, current_address = receive_socket.recvfrom(128)
+                receive_time = time.time() - time_sent
+                self.current_address = current_address[0]
+                try:
+                    self.current_name = socket.gethostbyaddr(
+                        self.current_address)[0]
+                except socket.error:
+                    self.current_name = self.current_address
+            except (KeyboardInterrupt, SystemExit):
+                sys.exit(1)
             except socket.error:
-                curr_name = curr_addr
-        except (KeyboardInterrupt, SystemExit):
-            sys.exit(1)
-        except socket.error:
-            pass
-        finally:
-            send_socket.close()
-            recv_socket.close()
+                pass
+            finally:
+                send_socket.close()
+                receive_socket.close()
 
-        if curr_addr is not None:
-            curr_host = "%s  (%s)" % (curr_name, curr_addr)
-        else:
-            curr_host = "*"
-        # print "%d\t%s" % (ttl, curr_host)
-        print("%d\t%s" % (ttl, curr_host))
+            if self.current_address is not None:
+                current_host = '{host}  ({address})'.format(
+                    host=self.current_name,
+                    address=self.current_address)
+            else:
+                current_host = '*'
 
-        ttl += 1
-        if curr_addr == dest_addr or ttl > max_hops:
-            break
+            print("{ttl}\t{host} {time}".format(ttl=ttl,
+                                                host=current_host,
+                                                time=receive_time * 1000))
+
+            if self.current_address == self.destination_address \
+                    or ttl > self.max_hops:
+                break
+            ttl += 1
+
+
+def main(destination_name):
+    trace = TraceRoute(destination_name)
+    trace.send()
 
 
 if __name__ == '__main__':
     try:
         main(sys.argv[1])
     except IndexError:
-        print("Usage: {0} www.google.com".format(sys.argv[0]))
-
+        print("Usage: {0} www.google.com".format(
+                os.path.basename(__file__)))
