4e203e88103347b921fc6502ee628316ac753ed4
Jason Grajeda <jason@grajeda.org>
2017-10-16 16:55:01 -0700

    Redesign with argparse.
    Added comments for functions.

--- system/rm_python.py
+++ system/rm_python.py
@@ -1,123 +1,91 @@
-"""Removes old versions of python on OSX.
+"""Removes links from old versions of python on OSX.
 
+Useful for removing links between differing versions without deleting the version
+itself.
 """
 
 import os
 import filecmp
-import sys
-import getopt
+import argparse
 
 
-def usage():
-    print("Usage: rm_python.py [options] ... [src path] [dst path]")
-    print("Options:")
-    print("{0:>10}{1:>8} : {2:<20}".format("-h", " ", "Usage"))
-    print("{0:>10}{1:>8} : {2:<20}".format("-p", " ", "Print links"))
-    print("{0:>10}{1:>8} : {2:<20}".format("-r", " ", "Remove links"))
-    print("{0:>10}{1:>8} : {2:<20}".format("-s", "--src=", "Source path"))
-    print("{0:>10}{1:>8} : {2:<20}".format("-d", "--dst=", "Destination path"))
+def get_options():
+    """Get commandline options from sys.argv and return dictionary of arguments.
+
+    Args:
+        -d: Optional boolean argument to flag links for removal.
+        version: A string version number of python to view or remove.
+        target: A path string containing links to python binaries, e.g. /usr/bin.
+    Returns:
+        A dictionary mapping of arguments.
+    """
+    parser = argparse.ArgumentParser(description="Remove old versions of python from OSX")
+    parser.add_argument("-d", help="delete links", action="store_true")
+    parser.add_argument("version", help="Python version", action="store", type=str)
+    parser.add_argument("target", help="Location of python links", action="store", type=str)
+    return vars(parser.parse_args())
 
 
-# def print_links(d1, d2, common, links, diff):
-def print_links(d1, d2, common):
-    # print("\nAll files:")
-    # for f in common:
-    #     print("{0}".format(os.path.join(d2, f)))
-    #
-    # print("\nLinks:")
-    # for f in links:
-    #     print("{0}".format(os.path.join(d2, f)))
-    #
-    # print("\nDifference:")
-    # for f in diff:
-    #     print("{0}".format(os.path.join(d2, f)))
+def print_links(links):
+    """print matched links in *target*.
 
-    match, mismatch, errors = filecmp.cmpfiles(d1, d2, common, shallow=False)
-    print("\nMatch:")
-    for f in match:
-        if os.path.islink(os.path.join(d2, f)):
-            print("link: {0}".format(os.path.join(d2, f))),
-            print("{0:>15}".format(os.readlink(os.path.join(d2, f))))
-        elif os.path.isfile(os.path.join(d2, f)):
-            print("file: {0}".format(os.path.join(d2, f)))
-
-    print("\nMismatch:")
-    for f in mismatch:
-        if os.path.islink(os.path.join(d2, f)):
-            print("link: {0}".format(os.path.join(d2, f)))
-            print("{0:>15}".format(os.readlink(os.path.join(d2, f))))
-        elif os.path.isfile(os.path.join(d2, f)):
-            print("file: {0}".format(os.path.join(d2, f)))
-    print("\nErrors", errors)
+    Args:
+        links: A list of links.
+    """
+    print("\nMatches:")
+    for f in links:
+        print(f, end='')
+        print("{0:>}".format(os.readlink(f)))
 
 
-def find_links(d1, d2):
-    d1_contents = set(os.listdir(d1))
-    d2_contents = set(os.listdir(d2))
-    common = list(d1_contents & d2_contents)
-    common_files = [f for f in common
-                    if os.path.isfile(os.path.join(d2, f))]
+def find_links(src, dst):
+    """Find symlinks and return list.
 
-    links = [f for f in common
-             if os.path.islink(os.path.join(d2, f))]
-    # cf = set(common_files)
-    # cl = set(links)
-    # diff = list(cf - cl)
+    Args:
+        src: A string containing the path to python
+        dst: A path string containing links from the src, e.g. /usr/bin.
+    Returns:
+        A list of link path strings.
+    """
+    dir_compare = filecmp.dircmp(src, dst)
+    links = [os.path.join(dst, link) for link in dir_compare.common_files
+             if os.path.islink(os.path.join(dst, link))]
 
-    return d1, d2, common_files, links
+    return links
 
 
-def rm_links(directory, links):
+def rm_links(links):
+    """Remove symlinks from *target* and print removals.
+
+    Args:
+        links: A list of link path strings.
+    Returns:
+        Prints removed links.
+    """
     try:
         for link in links:
-            os.remove(os.path.join(directory, link))
-            print("%s removed" % os.path.join(directory, link))
+            os.remove(link)
+            print("{link} removed".format(link=link))
     except OSError as e:
         print("\nCannot remove link!")
         print("Permissions Error ({0}): {1}".format(e.errno, e.strerror))
 
 
-def main(argv):
+def main():
+    """Program to remove symlinks of old python versions from target directory."""
     base = '/System/Library/Frameworks/Python.framework/Versions'
-    version = ''
-    bindir = 'bin'
-    # srcpath = os.path.join(base, version, bindir)
-    dstpath = ''
-    view_links = False
-    delete_links = False
-    try:
-        opts, args = getopt.getopt(argv, "prhs:d:", ["src=", "dst="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            usage()
-            sys.exit()
-        elif opt in ("-s", "--src"):
-            version = arg
-        elif opt in ("-d", "--dst"):
-            dstpath = arg
-        elif opt == "-p":
-            view_links = True
-        elif opt == "-r":
-            delete_links = True
 
-    srcpath = os.path.join(base, version, bindir)
+    args = get_options()
+    srcpath = os.path.join(base, args['version'], 'bin')
+    delete, target_path = args['d'], args['target']
 
-    if view_links:
-        # d1, d2, common, links, diff = find_links(srcpath, dstpath)
-        d1, d2, common, links = find_links(srcpath, dstpath)
-        print_links(d1, d2, common)
-        # print_links(d1, d2, common, links, diff)
-
-    elif delete_links:
-        _, d2, _, links = find_links(srcpath, dstpath)
-        rm_links(d2, links)
+    if delete:
+        links = find_links(srcpath, target_path)
+        rm_links(links)
     else:
-        usage()
-        sys.exit()
+        links = find_links(srcpath, target_path)
+        print_links(links)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
