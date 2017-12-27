7bc38e986a04ded6c5c6d331537c63f33ef605cd
Jason Grajeda <jason@grajeda.org>
2017-12-26 17:50:28 -0800

    Many changes to shorten code, however, data api is deaed.

--- finance/yahoo_stocks_data.py
+++ finance/yahoo_stocks_data.py
@@ -24,68 +24,99 @@ def get_stock_list(filename):
     # s = symbol(s) to lookup
     # f = flags (Symbol(s), price(l1), date(d1), time(t1), change(c1)) etc..
     # e = export??
+    # names = column names
     table_query = {
-        's': '',
-        'f': '',
-        'e': '.csv'
+        's': [],
+        'f': [],
+        'e': '.csv',
+        'column_names': []
     }
 
     # Name the columns
-    table_columns = []
+    # table_columns = []
 
     # Intermediate list to help create table_query.
-    flags = []
+    # flags = []
 
     with open(filename, 'r') as fh:
-        stock_list = []
-        sl = [tuple(line.split('\t', maxsplit=2)) for line in fh]
-        for index, element in enumerate(sl):
-            if element[0] == '\n':
-                start = index
+        # stock_list = []
+        # stock_list = [tuple(line.split('\t', maxsplit=2)) for line in fh]
+        # for index, element in enumerate(stock_list):
+        for element in fh:
+            element = tuple(element.strip().split(None, 1))
+            if not element or '#' in element[0]:
                 break
             else:
-                stock_list.append(element[0])
+                table_query['s'].append(element[0])
 
-        fl = sl[start + 1:]
-        for flag, name in fl:
-            table_columns.append(name.strip('\n'))
-            flags.append(flag)
+        # flags = stock_list[start + 1:]
+        for element in fh:
+            try:
+                flag, name = tuple(element.strip().split(None, 1))
+                if '#' in flag:
+                    continue
+                else:
+                    table_query['column_names'].append(name)
+                    table_query['f'].append(flag)
+            except ValueError:
+                continue
 
-        table_query['s'] = ','.join(stock_list)
-        table_query['f'] = ''.join(flags)
+        table_query['s'] = ','.join(table_query['s'])
+        table_query['f'] = ''.join(table_query['f'])
 
-    return table_query, table_columns
+    return table_query
 
 
-def build_yql_query(query, columns):
+def build_outside_query(yql_query_function):
+    def wrapper(*args, **kwargs):
+        query = yql_query_function(*args, **kwargs)
+        # payload for the final url, this is the fully built and encoded query sent to yahoo.
+        url_payload = {
+            'q': query,
+            'format': 'json'
+        }
+
+        baseurl = "https://query.yahooapis.com/v1/public/yql"
+        # Safe set to '', otherwise '/' characters in the query will not be encoded.
+        # quote_via used to set quote instead of quote_plus.
+        return '{base}?{query}'.format(
+            base=baseurl,
+            query=urllib.parse.urlencode(url_payload, safe='', quote_via=urllib.parse.quote)
+        )
+    return wrapper
+
+
+@build_outside_query
+def build_yql_query(query_data):
     """Returns a properly formatted url needed for a Yahoo YQL query"""
     base_table_url = 'http://download.finance.yahoo.com/d/quotes.csv?'
 
     # Parse the dictionary (query) as part of URL but do NOT encode ','.
     # Prevents double encoding down the line.
-    encoded_query = urllib.parse.urlencode(query, safe=',')
+    column_names = query_data.pop('column_names')
+    encoded_query = urllib.parse.urlencode(query_data, safe=',')
 
     # Combine base url with encoded query and yql statement
     table_url = '{base}{query}'.format(base=base_table_url, query=encoded_query)
-    yql_query = "select * from csv where url='{url}' and columns='{columns}'".format(
+    return "select * from csv where url='{url}' and columns='{columns}'".format(
         url=table_url,
-        columns=','.join(columns)
+        columns=','.join(column_names)
     )
 
-    # payload for the final url, this is the fully built and encoded query sent to yahoo.
-    url_payload = {
-        'q': yql_query,
-        'format': 'json'
-    }
+    # # payload for the final url, this is the fully built and encoded query sent to yahoo.
+    # url_payload = {
+    #     'q': yql_query,
+    #     'format': 'json'
+    # }
 
-    baseurl = "https://query.yahooapis.com/v1/public/yql"
+    # baseurl = "https://query.yahooapis.com/v1/public/yql"
 
-    # Safe set to '', otherwise '/' characters in the query will not be encoded.
-    # quote_via used to set quote instead of quote_plus.
-    return '{base}?{query}'.format(base=baseurl, query=urllib.parse.urlencode(
-        url_payload, safe='',
-        quote_via=urllib.parse.quote)
-    )
+    # # Safe set to '', otherwise '/' characters in the query will not be encoded.
+    # # quote_via used to set quote instead of quote_plus.
+    # return '{base}?{query}'.format(
+    #     base=baseurl,
+    #     query=urllib.parse.urlencode(url_payload, safe='', quote_via=urllib.parse.quote)
+    # )
 
 
 def dow10_highest_yielding(json_data):
@@ -140,19 +171,23 @@ def main():
     filename = os.path.join(dev, file_path, "stock_list.txt")
 
     # Build URL to pass to the YQL statement
-    table_query, table_columns = get_stock_list(filename)
-    url = build_yql_query(table_query, table_columns)
+    table_data = get_stock_list(filename)
+    url = build_yql_query(table_data)
     r = requests.get(url)
-    rows = r.json()['query']['results']['row']
+    try:
+        rows = r.json()['query']['results']['row']
 
     # Print results
-    print('Top 10 highest yielding Dow stocks sorted by yield and price')
-    print('{:>11}{:>11}{:>11}{:>11}'.format('symbol', 'price', 'div_share', 'yield'))
-    print('{0:>11}{0:>11}{0:>11}{0:>11}'.format('-----'))
-    price_dow = dow10_highest_yielding(rows)
-    for row in price_dow:
-        print('{symbol:>11}{price:>11}{div_share:>11}{yield:>11}'.format(**row))
-    print("\nUrl: {url}".format(url=r.url))
+        print('Top 10 highest yielding Dow stocks sorted by yield and price')
+        print('{:>11}{:>11}{:>11}{:>11}'.format('symbol', 'price', 'div_share', 'yield'))
+        print('{0:>11}{0:>11}{0:>11}{0:>11}'.format('-----'))
+        price_dow = dow10_highest_yielding(rows)
+
+        for row in price_dow:
+            print('{symbol:>11}{price:>11}{div_share:>11}{yield:>11}'.format(**row))
+        print("\nUrl: {url}".format(url=r.url))
+    except TypeError:
+        print(r.json())
 
 
 if __name__ == '__main__':
