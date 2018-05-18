2f0b95ec35d91ea6b6addf7b83cea49204242da0
Jason Grajeda <jason@grajeda.org>
2018-05-18 14:50:22 -0700

    Changes to docstrings.

--- system/password_tools.py
+++ system/password_tools.py
@@ -13,7 +13,9 @@ class User:
     @property
     def email(self):
         """Returns an email address contructed from first and last. Example:
-	first.last@example.com."""
+        first.last@example.com.
+
+        """
         return '{first}.{last}@example.com'.format(first=self.first.lower(),
                                                    last=self.last.lower())
 
@@ -25,10 +27,10 @@ class User:
     @full_name.setter
     def full_name(self, name):
         """Alternate way to set the properties, first and last.
-	
-	Args:
-	    name: string. Full name of the person with a space as the separator.
-	"""
+
+        Args:
+            name (str): Full name of the person with a space as the separator.
+        """
         first, last = name.split(' ')
         self.first = first
         self.last = last
@@ -37,7 +39,7 @@ class User:
         """Generates a random string of *length*.
 
         Args:
-            length: int. os.urandom returns bytes which is two
+            length (int): os.urandom returns bytes which is two
                 hexidecimal numbers, hence we divide by to to get
                 the correct length.
         """
@@ -48,12 +50,12 @@ class User:
 
     def generate_hash(self, hash_type='sha256'):
         """Generate hash from given string.
-	
-	Args:
-	    hash_type: string. optional. any of the acceptable hash algorithms supported by
-	        the hashlib module ('sha256', 'sha512', 'md5', 'dsa', etc), list available
-		using hashlib.algorithms_available.
-	"""
+
+        Args:
+        hash_type (str, optional): any of the acceptable hash algorithms supported by
+            the hashlib module ('sha256', 'sha512', 'md5', 'dsa', etc), list available
+            using hashlib.algorithms_available. Defaults to sha256.
+        """
         if hash_type in hashlib.algorithms_available:
             h = hashlib.new(hash_type)
             h.update(self.password.encode('utf-8'))
@@ -67,7 +69,7 @@ def generate_random_string(length=8):
     """Generates a random string of *length*.
 
     Args:
-        length: integer. os.urandom returns bytes which is two
+        length (int, optional): os.urandom returns bytes which is two
             hexidecimal numbers, hence we divide by to to get
             the correct length.
     """
@@ -78,15 +80,19 @@ def generate_random_string(length=8):
     return ''.join(random.sample(usable_characters, length))
 
 
-def generate_hash(self, hash_type='sha256'):
-        """Generate hash from given string"""
-        if hash_type in hashlib.algorithms_available:
-            h = hashlib.new(hash_type)
-            h.update(self.password.encode('utf-8'))
-            return h.hexdigest()
-        else:
-            print('Incorrect hash type. Choose from:\n {}'.format(hashlib.algorithms_available))
-            return
+def generate_hash(password, hash_type='sha256'):
+    """Generate hash from given string.
+
+    Args:
+        hash_type (str, optional): Defaults to sha256, other options are sha512, md5, or dsa.
+    """
+    if hash_type in hashlib.algorithms_available:
+        h = hashlib.new(hash_type)
+        h.update(password.encode('utf-8'))
+        return h.hexdigest()
+    else:
+        print('Incorrect hash type. Choose from:\n {}'.format(hashlib.algorithms_available))
+        return
 
 
 def main():
