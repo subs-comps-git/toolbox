a336a09f6d509bb781679a496a4fa54af0c460e5
Jason Grajeda <jason@grajeda.org>
2018-09-16 13:49:22 -0700

    Main program to collect data in game_data module

---
+++ web_scraping/game_data/collect_data.py
@@ -0,0 +1,65 @@
+"""Main program to collect swgoh guild data
+
+
+"""
+
+
+import sqlite3
+import get_gp_data as gd
+import load_sql as load
+
+
+def connect_db(db_name):
+    db_uri = f'file:{db_name}?mode=rw'
+    try:
+        conn = sqlite3.connect(db_uri, uri=True)
+        cursor = conn.cursor()
+        cursor.execute(
+            """SELECT name
+             FROM sqlite_master
+             WHERE type='table' AND name = 'gp'
+             ORDER BY 1"""
+        )
+        if cursor.fetchone():
+            print('Found tables')
+            return conn
+        else:
+            print("No tables found.")
+            raise sqlite3.DatabaseError
+    except sqlite3.DatabaseError:
+        conn = sqlite3.connect(db_name)
+        load.create_schema(conn, '../swgoh.sql')
+        return conn
+
+
+def main():
+    """Collect data from swgoh.gg and load database.
+
+    """
+
+    # Guild data website.
+    url = 'https://swgoh.gg/g/11008/hansoloshotfirst/gp/'
+    # SQL
+    insert_gp = """
+        INSERT INTO gp
+         (u_id, dl_date, total_gp, char_gp, ship_gp)
+        VALUES
+         ((SELECT uid FROM users WHERE name = ?), (strftime('%s', 'now')), ?, ?, ?);
+        """
+    insert_names = 'INSERT INTO users (name) VALUES (?);'
+
+    # Collect data
+    html_data = gd.get_html(url)
+    swgoh_soup = gd.make_soup(html_data, 'html5lib')
+    gp = gd.make_gp_list(swgoh_soup)
+
+    # Load database
+    db_name = 'swgoh.db'
+    db_schema = 'swgoh.sql'
+    conn = load.setup_database(db_name, db_schema)
+    load.load_data(conn, gp, insert_gp, insert_names)
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
